b0cf590e0dbc22a05a81e0f0d124f03b8a8f36f6
visnia <lepetinez@gmail.com>
2019-04-28 14:12:50 +0200

    DAGGER ANDROID PART1

--- app/src/main/java/c/lepetinez/quizapplication/main/view/MainActivity.kt
+++ app/src/main/java/c/lepetinez/quizapplication/main/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package c.lepetinez.quizapplication
+package c.lepetinez.quizapplication.main.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import c.lepetinez.quizapplication.R
 
 class MainActivity : AppCompatActivity() {
 
