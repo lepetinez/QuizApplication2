bfdad6a42fee4f1c0e7a5065b14e54df4ecb6124
visnia <lepetinez@gmail.com>
2019-04-28 19:50:46 +0200

    add_rx_schedulers

---
+++ app/src/main/java/c/lepetinez/quizapplication/common/rx/RxSchedulers.kt
@@ -0,0 +1,12 @@
+package c.lepetinez.quizapplication.common.rx
+
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+open class RxSchedulers {
+
+    open fun mainThread() = AndroidSchedulers.mainThread()
+    open fun io() = Schedulers.io()
+    open fun computation() = Schedulers.computation()
+    fun trampoline() = Schedulers.trampoline()
+}
