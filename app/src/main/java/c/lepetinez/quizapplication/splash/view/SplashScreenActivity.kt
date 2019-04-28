1b0ccef6b57cec8972529611f32d024afd14b9c5
visnia <lepetinez@gmail.com>
2019-04-28 19:07:23 +0200

    edited

--- app/src/main/java/c/lepetinez/quizapplication/splash/view/SplashScreenActivity.kt
+++ app/src/main/java/c/lepetinez/quizapplication/splash/view/SplashScreenActivity.kt
@@ -1,10 +1,10 @@
-package c.lepetinez.quizapplication.main.view
+package c.lepetinez.quizapplication.splash.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import c.lepetinez.quizapplication.R
 
-class MainActivity : AppCompatActivity() {
+class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
