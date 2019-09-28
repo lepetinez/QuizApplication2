a3948be009c7a33937773c73ecf3ddc7b1565ee6
visnia <lepetinez@gmail.com>
2019-09-28 23:09:39 +0200

    android x migration

--- app/src/main/java/c/lepetinez/quizapplication/splash/view/SplashScreenActivity.kt
+++ app/src/main/java/c/lepetinez/quizapplication/splash/view/SplashScreenActivity.kt
@@ -1,6 +1,6 @@
 package c.lepetinez.quizapplication.splash.view
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import c.lepetinez.quizapplication.R
 
